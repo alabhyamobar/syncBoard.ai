768b02aef40a11b173ab4bbfcd02bf030d586f8f
alabhyamobar <alabhyamobar50@gmail.com>
2026-07-15 02:04:28 +0530

    render configurations

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" , "https://sync-board-ai.vercel.app"],
     credentials: true, 
   }),
 );
